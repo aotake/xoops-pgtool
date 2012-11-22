ff4d370356ce1c01f01b2103d66d8a8d70b2d1b1
aotake <aotake@bmath.org>
2012-11-22 11:58:57 +0900

    16進値対応, unsigned の対応型の変更, INDEX の修正, bigserial の追加,blob 関連のbytea 型適用

--- my2pg.php
+++ my2pg.php
@@ -15,6 +15,9 @@ function printSql(&$str)
         // 連結
         $sql = implode(",\n", $str)."\n";
 
+        // バッククォートを除去
+        $sql = str_replace("`", "", $sql);
+
         // CREATE TABLE hoge (, id int .....) のようなやつから最初の , を除去
         $sql = preg_replace("/([^\(]+)\(\s*,(.+)/siU", "\\1(\\2", $sql);
         // CREATE TABLE hoge (id int .....  aaa int, ); のようなやつから最後の , を除去
@@ -73,12 +76,36 @@ if ($handle) {
         }
 
         // テーブル名取得
-        if(preg_match("/CREATE TABLE\s*([^\(]+)\(.*/", ltrim($buffer), $m)){
+        if(preg_match("/CREATE TABLE\s*`?([^\(`]+)`?\s*\(.*/", ltrim($buffer), $m)){
             $table = trim($m[1]);
         }
 
         // ENGINE=MyISAM があれば削除
         $buffer = preg_replace("/(.+)ENGINE\s*=\s*MyISAM(.+)/", "\\1\\2", $buffer);
+        // TYPE=MyISAM があれば削除（たぶん古いタイプ）
+        $buffer = preg_replace("/(.+)TYPE\s*=\s*MyISAM(.+)/", "\\1\\2", $buffer);
+
+        // varchar(xx) binary は varchar(xx) に
+        $buffer = preg_replace("/(.+)\s(varchar\(\d+\))\s+binary\s+(.*)/", "\\1 \\2 \\3", $buffer);
+
+
+        // int(xx) unsigned は bigint に
+        $buffer = preg_replace("/(.+)\s+(int)\(\d+\) unsigned(.*)/", "\\1 bigint\\3", $buffer);
+
+        // mediumint(xx) unsigned は int に
+        $buffer = preg_replace("/(.+)\s+(mediumint)\(\d+\) unsigned(.*)/", "\\1 int\\3", $buffer);
+
+        // mediumint unsigned は int に
+        $buffer = preg_replace("/(.+)\s+(mediumint)\s+unsigned(.*)/", "\\1 int \\3", $buffer);
+
+        // smallint(xx) unsigned は int に
+        $buffer = preg_replace("/(.+)\s(smallint)\(\d+\) unsigned(.*)/", "\\1 int \\3", $buffer);
+
+        // tinyint(xx) unsigned は smallint に
+        $buffer = preg_replace("/(.+)\s(tinyint)\(\d+\) unsigned(.*)/", "\\1 smallint \\3", $buffer);
+
+        // tinyint unsigned は smallint に
+        $buffer = preg_replace("/(.+)\s(tinyint)\s+unsigned(.*)/", "\\1 smallint \\3", $buffer);
 
         // int(xx), mediumint(xx) は int に
         $buffer = preg_replace("/(.+)\s+(int|mediumint)\(\d+\)(.*)/", "\\1 int\\3", $buffer);
@@ -92,17 +119,37 @@ if ($handle) {
         // tinyint は smallint に
         $buffer = preg_replace("/(.+)\s(tinyint)\s+(.*)/", "\\1 smallint \\3", $buffer);
 
+
+
+
+
+        // 16進数表記のしかたを変更 mysql=> 0xFFFF, pgsq => X'FFFF'::int
+        $buffer = preg_replace("/0x([0-9a-fA-F]+)/", "X'\\1'::int", $buffer);
+
         // unsigned は削除
-        $buffer = preg_replace("/(.+)(unsigned)(.*)/", "\\1\\3", $buffer);
+        //$buffer = preg_replace("/(.+)(unsigned)(.*)/", "\\1\\3", $buffer);
 
         // mediumtext は text に
         $buffer = preg_replace("/(.+)\s+(mediumtext)(.*)/", "\\1 text\\3", $buffer);
+
+        // binary,blob,tinyblob,mediumblob,longblob は bytea に
+        $buffer = preg_replace("/(.+)\s+(binary|tinyblob|mediumblob|longblob|blob)(.*)/", "\\1 bytea\\3", $buffer);
+
+        // binary(N) は bytea に
+        $buffer = preg_replace("/(.+)\s+(binary)\(\d+\)(.*)/", "\\1 bytea\\3", $buffer);
 
         // auto_increment は serial に変更,  serial キーのカラムを保存
         $buffer = preg_replace("/([^\s]+)\s+(int|smallint)(.*)auto_increment/", "\\1 serial", $buffer);
         if(preg_match("/([^\s]+)\s+serial/", $buffer, $m)){
             $primary_keys[$table] = $m[1];
         }
+
+        // bigint の auto_increment は bigserial に変更,  serial キーのカラムを保存
+        $buffer = preg_replace("/([^\s]+)\s+(bigint)(.*)auto_increment/", "\\1 bigserial", $buffer);
+        if(preg_match("/([^\s]+)\s+bigserial/", $buffer, $m)){
+            $primary_keys[$table] = $m[1];
+        }
+
 
         // 16進数表記の変更 0xXXXX -> X'XXXX'::integer （註：文字列型のとき問題）
         $hex_pattern = "(.+)0x([0-9a-fA-F]+)(.*)";
@@ -113,17 +160,66 @@ if ($handle) {
 
         // KEY(xxx) は削除。CREATE INDEX 用に管理。
         $key_pattern = "\s*KEY\s*\((.+)\)";
-        if(preg_match("/$key_pattern/", $buffer, $m) && !preg_match("/UNIQUE/", $buffer)){
-            $index[$table][] = $m[1];
+        if(preg_match("/$key_pattern/", $buffer, $m) && !preg_match("/(PRIMARY|UNIQUE)/", $buffer)){
+            $index[$table][] = "\"".str_replace(",", "\",\"", $m[1])."\"";
+            continue;
+        }
+
+        // KEY keyname (xxx) は削除。CREATE INDEX 用に管理。
+        $key_pattern = "\s*KEY\s*`?([^`]+)`?\s*\(`?([^`]+)`?\)";
+        if(preg_match("/$key_pattern/", $buffer, $m) && !preg_match("/(PRIMARY|UNIQUE)/", $buffer)){
+            //$index_with_name[$table][$m[1]] = "\"".$m[2]."\"";
+            $index[$table][] = "\"".$m[2]."\"";
+            continue;
+        }
+
+        // KEY keyname (xxx(NN)) は削除。CREATE INDEX 用に管理。
+        //   MySQL は text 型は長さをしていしてvarchar的なインデックスがつくれるらしい
+        //   PostgreSQL は text 型にもインデックスがはれるので (NN) の部分を除去する
+        $key_pattern = "\s*KEY\s*`?([^`]+)`?\s*\("
+            ."("
+            .   "`?([^`]+)`?\(\d+\)"
+            .")"
+            ."("
+            .   "\s*,\s*`?([^`]+)`?\(\d+\)"
+            ."){0,}"
+            ."\)";
+        if(preg_match("/$key_pattern/", $buffer, $m) && !preg_match("/(PRIMARY|UNIQUE)/", $buffer)){
+            $keys = null;
+            for($i = 3; $i < count($m); $i++){
+                if($i % 2 == 1){
+                    if($keys == ""){
+                        $keys = "\"".$m[$i]."\"";
+                    } else {
+                        $keys .= ",\"".$m[$i]."\"";
+                    }
+                }
+            }
+            $index_with_name[$table][$m[1]] = $keys;
             continue;
         }
 
         // UNIQUE KEY(xxx) は削除。CREATE INDEX 用に管理。
         $uniq_pattern = "\s*UNIQUE KEY\s*\((.+)\)";
-        if( preg_replace("/$uniq_pattern/", $buffer, $m) ){
-            $unique[$table][] = $m[1];
+        if( preg_match("/$uniq_pattern/", $buffer, $m) ){
+            $unique[$table][] = "\"".$m[1]."\"";
             continue;
         }
+        // UNIQUE KEY key_name (xxx) は削除。CREATE INDEX 用に管理。
+        $uniq_pattern = "\s*UNIQUE KEY\s*`?([^`]+)`?\s*\(`?([^`]+)`?\)";
+        if( preg_match("/$uniq_pattern/", $buffer, $m) ){
+            //$unique_with_name[$table][$m[1]] = "\"".$m[2]."\"";
+            $unique[$table][] = "\"".$m[2]."\"";
+            continue;
+        }
+
+        // カラム名に " をつける
+        if(preg_match("/(\s*)([^\s\"]+)\s+(.+)/", $buffer, $m)){
+            if(!in_array(trim($m[2]), array("CREATE", "INSERT", "ALTER", "UPDATE", "PRIMARY",")"))){
+                $buffer = $m[1]."\"".$m[2]."\" ". $m[3];
+            }
+        }
+
 
         // 行末の "," は削除する（出力時に連結）
         $buffer = preg_replace("/(.+),$/", "\\1", $buffer);
@@ -142,15 +238,49 @@ if ($handle) {
 printSql($str);
 
 // CREATE INDEX 文
-foreach($index as $tbl => $key_array){
-    foreach($key_array as $k){
-        print "CREATE INDEX ".$tbl."_".str_replace(",", "_", $k)."_idx ON $tbl ($k);\n";
+if(isset($index)){
+    foreach($index as $tbl => $key_array){
+        foreach($key_array as $k){
+            $sql = "CREATE INDEX \"".$tbl."_".str_replace(",", "_", str_replace('"', '', $k))."_idx\" ON \"$tbl\" ($k);\n";
+            // バッククォートを除去
+            $sql = str_replace("`", "", $sql);
+            print $sql;
+        }
+    }
+}
+// CREATE INDEX 文(名前付き）
+if(isset($index_with_name)){
+    foreach($index_with_name as $tbl => $key_array){
+        foreach($key_array as $kname => $kval){
+            $sql = "CREATE INDEX \"".$kname."\" ON \"$tbl\" ($kval);\n";
+            // バッククォートを除去
+            $sql = str_replace("`", "", $sql);
+            print $sql;
+        }
     }
 }
 // CREATE UNIQUE INDEX 文
-foreach($unique as $tbl => $key_array){
-    foreach($key_array as $k){
-        print "CREATE UNIQUE INDEX ".$tbl."_".str_replace(",", "_", $k)."_idx ON $tbl ($k);\n";
+if(isset($unique)){
+    foreach($unique as $tbl => $key_array){
+        foreach($key_array as $k){
+            $ks = str_replace("\"", "", $k); // 付け足してしまった " を一端除去
+            $ks = explode(",", $ks);         // カンマで分割
+            $sql = "CREATE UNIQUE INDEX ".$tbl."_".implode("_", $ks)."_idx ON $tbl (\"".implode("\",\"",$ks)."\");\n";
+            // バッククォートを除去
+            $sql = str_replace("`", "", $sql);
+            print $sql;
+        }
+    }
+}
+// CREATE UNIQUE INDEX 文（名前付き）
+if(isset($unique_with_name)){
+    foreach($unique_with_name as $tbl => $key_array){
+        foreach($key_array as $k => $v){
+            $sql = "CREATE UNIQUE INDEX \"".$k."\" ON \"$tbl\" ($v);\n";
+            // バッククォートを除去
+            $sql = str_replace("`", "", $sql);
+            print $sql;
+        }
     }
 }
 //print_r($unique);
